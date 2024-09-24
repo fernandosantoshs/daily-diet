0eb9cc940df2ce198fd8d77cb3ebfa68fd6e19e7
Fernando Santos <fernandosantoshs@gmail.com>
2024-09-24 12:12:12 -0300

    Feat: Created get specific meal, put and delete routes; updated Readme

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 ### Regras da aplicação
 
-- Deve ser possível criar um usuário
-- Deve ser possível identificar o usuário entre as requisições
-- Deve ser possível registrar uma refeição feita, com as seguintes informações:
-  _As refeições devem ser relacionadas a um usuário._
+- [x] Deve ser possível criar um usuário / It should be possible create an user
+- [x] Deve ser possível identificar o usuário entre as requisições
+- [x] Deve ser possível registrar uma refeição feita, com as seguintes informações:
+      _As refeições devem ser relacionadas a um usuário._
   - Nome
   - Descrição
   - Data e Hora
   - Está dentro ou não da dieta
 - Deve ser possível editar uma refeição, podendo alterar todos os dados acima
 - Deve ser possível apagar uma refeição
-- Deve ser possível listar todas as refeições de um usuário
-- Deve ser possível visualizar uma única refeição
+- [x] Deve ser possível listar todas as refeições de um usuário
+- [x] Deve ser possível visualizar uma única refeição
 - Deve ser possível recuperar as métricas de um usuário
   - Quantidade total de refeições registradas
   - Quantidade total de refeições dentro da dieta
--- src/routes/mealsRoutes.ts
+++ src/routes/mealsRoutes.ts
@@ -40,15 +40,102 @@ export async function mealsRoutes(app: FastifyInstance) {
 
   app.get('/', { preHandler: [checkIfUserExists] }, async (request, reply) => {
     const sessionId = request.cookies.sessionId;
+
     const user = await knex('users')
       .where({ session_id: sessionId })
       .select('id')
       .first();
 
-    console.log(user.id);
-
     const meals = await knex('meals').where({ user_id: user.id }).select('*');
 
     return reply.status(200).send(meals);
   });
+
+  app.get(
+    '/:mealId',
+    { preHandler: [checkIfUserExists] },
+    async (request, reply) => {
+      const paramsSchema = z.object({ mealId: z.string().uuid() });
+
+      const sessionId = request.cookies.sessionId;
+
+      const user = await knex('users')
+        .where({ session_id: sessionId })
+        .select('id')
+        .first();
+
+      const { mealId } = paramsSchema.parse(request.params);
+
+      const meal = await knex('meals')
+        .where({ user_id: user.id, id: mealId })
+        .select('*')
+        .first();
+
+      if (!meal) {
+        return reply.status(404).send('Meal not found');
+      }
+
+      return reply.status(200).send(meal);
+    }
+  );
+
+  app.put(
+    '/:mealId',
+    { preHandler: [checkIfUserExists] },
+    async (request, reply) => {
+      const paramsSchema = z.object({ mealId: z.string().uuid() });
+
+      const putMealBodySchema = z.object({
+        name: z.string().optional(),
+        description: z.string().optional(),
+        isOnDiet: z.boolean().optional(),
+        date: z.coerce.date().optional(),
+      });
+
+      const { mealId } = paramsSchema.parse(request.params);
+
+      const { name, description, isOnDiet, date } = putMealBodySchema.parse(
+        request.body
+      );
+
+      const meal = await knex('meals').where({ id: mealId }).first();
+
+      if (!meal) {
+        return reply.status(404).send('Meal not found');
+      }
+
+      const updatedMeal = {
+        ...meal,
+        name: name ?? meal.name,
+        description: description ?? meal.description,
+        is_on_diet: isOnDiet ?? meal.is_on_diet,
+        date: date ?? meal.date,
+        updated_at: new Date().toLocaleString('pt-BR'),
+      };
+
+      await knex('meals').where('id', mealId).update(updatedMeal);
+
+      return reply.status(200).send(updatedMeal);
+    }
+  );
+
+  app.delete(
+    '/:mealId',
+    { preHandler: [checkIfUserExists] },
+    async (request, reply) => {
+      const paramsSchema = z.object({ mealId: z.string().uuid() });
+
+      const { mealId } = paramsSchema.parse(request.params);
+
+      const meal = await knex('meals').where('id', mealId).first();
+
+      if (!meal) {
+        return reply.status(404).send('Meal not found');
+      }
+
+      await knex('meals').where('id', mealId).del();
+
+      return reply.status(204).send();
+    }
+  );
 }
