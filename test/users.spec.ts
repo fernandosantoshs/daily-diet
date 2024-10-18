a5bed64df542460545693ac8bfa164c15957d5b8
Fernando Santos <fernandosantoshs@gmail.com>
2024-10-18 09:41:38 -0300

    Test route post users; Add .env.test file

---
+++ test/users.spec.ts
@@ -0,0 +1,26 @@
+import { it, describe, beforeAll, afterAll, beforeEach } from 'vitest';
+import { app } from '../src/app';
+import request from 'supertest';
+import { execSync } from 'child_process';
+
+describe('Users routes', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  beforeEach(async () => {
+    execSync('npm run knex --- migrate:latest');
+    execSync('npm run knex --- migrate:rollback --all');
+  });
+
+  it('should be able to create an user', async () => {
+    await request(app.server)
+      .post('/users')
+      .send({ name: 'John Wick', email: 'johnwick@continentalhotel.com' })
+      .expect(201);
+  });
+});
